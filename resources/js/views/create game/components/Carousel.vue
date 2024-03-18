af9c6f7aaf10250e819bee34ca3321667a663f0f
RBNJG <rubenjordan1a@gmail.com>
2024-03-18 17:48:37 +0100

    Added vueper-slides to carousel

--- resources/js/views/create game/components/Carousel.vue
+++ resources/js/views/create game/components/Carousel.vue
@@ -1,144 +1,53 @@
 <template>
     <div class="carousel">
-        <div class="d-flex justify-content-between">
-            <button @click="prevImage" class="arrow left-arrow">&lt;</button>
-            <div class="image-container" style="position: relative; overflow: hidden;">
-                <div class="image-container" style="position: relative; overflow: hidden;">
-                    <transition :name="transitionName" mode="out-in">
-                        <img :src="images[currentImage]" :alt="'Carousel image ' + currentImage" :key="currentImage"
-                            class="carousel-image">
-                    </transition>
-                </div>
-            </div>
-            <button @click="nextImage" class="arrow right-arrow">&gt;</button>
-        </div>
-        <div class="dots">
-            <span v-for="(image, index) in images" :key="index" class="dot" @click="() => setImage(index)"
-                :class="{ active: currentImage === index }"></span>
-        </div>
+        <vueper-slides :dragging-distance="50" class="no-shadow" fixed-height="28rem" slide-image-inside>
+            <vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide.image" />
+        </vueper-slides>
     </div>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue';
+import { VueperSlides, VueperSlide } from 'vueperslides';
+import 'vueperslides/dist/vueperslides.css';
 
-export default {
-    setup() {
-        const currentImage = ref(0);
-        const images = ref([
-            '/storage/bob-esponja.jpg',
-            '/storage/pikachu.jpg',
-            '/storage/robot.jpg',
-        ]);
-        const transitionName = ref('slide-right');
-
-        const nextImage = () => {
-            transitionName.value = 'slide-right';
-            currentImage.value = (currentImage.value + 1) % images.value.length;
-        };
-
-        const prevImage = () => {
-            transitionName.value = 'slide-left';
-            currentImage.value = (currentImage.value + images.value.length - 1) % images.value.length;
-        };
-
-        const setImage = (index) => {
-            direction.value = index > currentImage.value ? 'right' : 'left';
-            currentImage.value = index;
-        };
-
-        return { currentImage, images, nextImage, prevImage, setImage, transitionName };
+const slides = ref([
+    {
+        title: 'Bob',
+        image: '/storage/bob-esponja.jpg'
     },
-};
+    {
+        title: 'Pikachu',
+        image: '/storage/pikachu.jpg'
+    },
+    {
+        title: 'Robot',
+        image: '/storage/robot.jpg'
+    }
+])
+
 </script>
 
-<style scoped>
-.carousel {
-    width: 100%;
-    height: 100%;
+<style>
+.vueperslides__arrow {
+    color: black;
 }
 
-.image-container {
-    width: auto;
-    height: 25rem;
+.vueperslides__arrow svg {
+    stroke-width: 2;
 }
 
-.carousel-image {
-    width: 100%;
-    height: 100%;
-    animation-duration: 0.5s;
+.vueperslides__bullet .default {
+  background-color: #FFF;
+  border: 2px solid #A52FDD;
+  box-shadow: 0px 2px 2px 1px rgba(0,0,0,0.3);
+  transition: 0.3s;
+  width: 16px;
+  height: 16px;
 }
 
-.arrow {
-    font-size: 32px;
-    background-color: #ffffff00;
-    border: none;
-    cursor: pointer;
-}
-
-.left-arrow {
-    left: 10px;
-}
-
-.right-arrow {
-    right: 10px;
-}
-
-.dots {
-    text-align: center;
-    padding-top: 20px;
-}
-
-.dot {
-    cursor: pointer;
-    height: 15px;
-    width: 15px;
-    margin: 0 5px;
-    background-color: #FFF;
-    border-radius: 50%;
-    border: #A52FDD solid 3px;
-    display: inline-block;
-    box-shadow: 0px 4px 3px 0px rgba(0, 0, 0, 0.3);
-}
-
-.dot.active {
+.vueperslides__bullet--active .default 
+{
     background-color: #A52FDD;
-}
-
-@keyframes slideInLeft {
-    0% {
-        transform: translateX(-100%);
-    }
-
-    100% {
-        transform: translateX(0);
-    }
-}
-
-@keyframes slideInRight {
-    0% {
-        transform: translateX(100%);
-    }
-
-    100% {
-        transform: translateX(0);
-    }
-}
-
-.slide-left-enter-active, .slide-right-enter-active {
-  animation-duration: 0.5s;
-  position: absolute;
-  width: 100%;
-}
-.slide-left-leave-active, .slide-right-leave-active {
-  animation-duration: 0.5s;
-  position: absolute;
-  width: 100%;
-}
-.slide-left-enter, .slide-right-leave-to /* left to right */ {
-  transform: translateX(100%);
-}
-.slide-right-enter, .slide-left-leave-to /* right to left */ {
-  transform: translateX(-100%);
 }
 </style>
