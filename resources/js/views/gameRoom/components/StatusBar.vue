dc13a5e601bed947f7fc24cd881bd7e2f1840c45
Fabian <fabian@prueba.com>
2024-03-19 18:36:39 +0100

    commit

---
+++ resources/js/views/gameRoom/components/StatusBar.vue
@@ -0,0 +1,23 @@
+<!-- * Component for information in game -->
+
+<template>
+    <div class="container flex flex-column bar-container">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.bar-container
+{
+    background-color: #FD6F5A;
+    width: 400px;
+    height: 400px;
+    border-radius: 12px 12px 0 0;
+    z-index: 100;
+}
+</style>
