2168305a557fa66569034b8f9a8766dc1c901dbc
Fabian <fabian@prueba.com>
2024-03-20 19:21:10 +0100

    setup for info-players done

--- resources/js/views/gameRoom/components/InfoPlayers.vue
+++ resources/js/views/gameRoom/components/InfoPlayers.vue
@@ -1,8 +1,37 @@
 <!-- * Component for info players -->
 
 <template>
-    <div class="jugadores-container">
-        
+    <div class="p-2 flex flex-column justify-content-start align-items-center players-container">
+        <div class="p-2 flex flex-column align-items-center player">
+            <div class="p-0 m-0 avatar" style="width: 40px; min-height: 40px; border-radius:50px; background-color:red;"></div>
+            <p class="name-player-info">SUPERPICASO69</p>
+            <p class="points-player-info">560 pts.</p>
+        </div>
+        <div class="p-2 flex flex-column align-items-center player">
+            <div class="p-0 m-0 avatar" style="width: 40px; min-height: 40px; border-radius:50px; background-color:red;"></div>
+            <p class="name-player-info">DA_VINCI33</p>
+            <p class="points-player-info">560 pts.</p>
+        </div>
+        <div class="p-2 flex flex-column align-items-center player">
+            <div class="p-0 m-0 avatar" style="width: 40px; min-height: 40px; border-radius:50px; background-color:red;"></div>
+            <p class="name-player-info">MEGA_DRAWER20</p>
+            <p class="points-player-info">560 pts.</p>
+        </div>
+        <div class="p-2 flex flex-column align-items-center player">
+            <div class="p-0 m-0 avatar" style="width: 40px; min-height: 40px; border-radius:50px; background-color:red;"></div>
+            <p class="name-player-info">MEGA_DRAWER20</p>
+            <p class="points-player-info">560 pts.</p>
+        </div>
+        <div class="p-2 flex flex-column align-items-center player">
+            <div class="p-0 m-0 avatar" style="width: 40px; min-height: 40px; border-radius:50px; background-color:red;"></div>
+            <p class="name-player-info">MEGA_DRAWER20</p>
+            <p class="points-player-info">560 pts.</p>
+        </div>
+        <div class="p-2 flex flex-column align-items-center player">
+            <div class="p-0 m-0 avatar" style="width: 40px; min-height: 40px; border-radius:50px; background-color:red;"></div>
+            <p class="name-player-info">MEGA_DRAWER20</p>
+            <p class="points-player-info">560 pts.</p>
+        </div>
     </div>
 </template>
 
@@ -11,10 +40,35 @@
 
 <style scoped>
 
-.jugadores-container {
+.name-player-info
+{
+    font-family: 'Lilita One', sans-serif;
+    font-size: 10px;
+    margin: 0;
+    padding: 0;
+}
+
+.points-player-info
+{
+    font-family: 'Lilita One', sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+.players-container {
     width: 100%;
     height: 100%;
     background-color: #FFFDFD;
     border-radius: 22px;
+}
+
+.player
+{
+    height: 95px;
+    width: 100%;
+    border: 3px solid purple;
+    border-radius: 22px;
+    margin-bottom: 6px;
+    box-shadow: 0 4px 4px rgba(0,0,0,.25);
 }
 </style>
