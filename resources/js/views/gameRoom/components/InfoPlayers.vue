4c56966c7c653e64cb12a8afdd7838ea8d895fc1
Fabian <fabian@prueba.com>
2024-03-20 19:01:01 +0100

    added style for info-player

--- resources/js/views/gameRoom/components/InfoPlayers.vue
+++ resources/js/views/gameRoom/components/InfoPlayers.vue
@@ -1,11 +1,20 @@
 <!-- * Component for info players -->
 
 <template>
-
+    <div class="jugadores-container">
+        
+    </div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+
+.jugadores-container {
+    width: 100%;
+    height: 100%;
+    background-color: #FFFDFD;
+    border-radius: 22px;
+}
 </style>
