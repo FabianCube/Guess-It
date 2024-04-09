edad7574215855d0fd8f25c79b2cc3ecffc0166a
Fabian <fabian@prueba.com>
2024-04-09 16:55:29 +0200

    replaced deprecated format of script

--- resources/js/views/gameRoom/components/ChatMessages.vue
+++ resources/js/views/gameRoom/components/ChatMessages.vue
@@ -1,36 +1,48 @@
 <template>
-    <ul class="chat" ref="chatContainer">
-      <li class="left clearfix" v-for="message in messages" :key="message.id">
-        <div class="clearfix">
-          <div class="header">
-            <strong>
-              {{ message.user.name }}
-            </strong>
-          </div>
-          <p>
-            {{ message.message }}
-          </p>
+  <ul class="chat" ref="chatContainer">
+    <li class="left clearfix" v-for="message in messages" :key="message.id">
+      <div class="clearfix">
+        <div class="header">
+          <strong>
+            {{ message.user.name }}
+          </strong>
         </div>
-      </li>
-    </ul>
-  </template>
-  <script>
-  export default {
-    props: ["messages"],
-    updated() {
-      this.$nextTick(() => {
-        const container = this.$refs.chatContainer;
-        container.scrollTop = container.scrollHeight;
-      });
-    },
-  };
-  </script>
-  <style scoped>
-  
-  .chat{
-    height: 100%;
-    width: 100%;
-    overflow-y: auto;
-  }
-  
-  </style>
+        <p>
+          {{ message.message }}
+        </p>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { onUpdated } from 'vue';
+
+const props = defineProps([ 'messages' ])
+
+onUpdated()
+{
+  this.$nextTick(() => {
+    const container = this.$refs.chatContainer;
+    container.scrollTop = container.scrollHeight;
+  });
+}
+
+// ============ DEPRECATED ==========
+// export default {
+//   props: ["messages"],
+//   updated() {
+//     this.$nextTick(() => {
+//       const container = this.$refs.chatContainer;
+//       container.scrollTop = container.scrollHeight;
+//     });
+//   },
+// };
+</script>
+<style scoped>
+.chat {
+  height: 100%;
+  width: 100%;
+  overflow-y: auto;
+}
+</style>
