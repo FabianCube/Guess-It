1379ccb08c9380b99433abfdc4431194e2ea9916
Fabian <fabian@prueba.com>
2024-04-16 17:57:08 +0200

    WORKING IN API

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::post('logout', [AuthenticatedSessionController::class, 'logout']);
 // Route::get('/chat', [App\Http\Controllers\ChatsController::class, 'index']);
 // Route::get('/messages', [App\Http\Controllers\ChatsController::class, 'fetchMessages']);
 // Route::post('/messages', [App\Http\Controllers\ChatsController::class, 'sendMessage']);
-Route::get('/canvas', [App\Http\Controllers\ChatsController::class, 'fetchCanvas']);
-Route::post('/canvas', [App\Http\Controllers\ChatsController::class, 'sendCanvas']);
+// Route::get('/canvas', [App\Http\Controllers\ChatsController::class, 'fetchCanvas']);
+// Route::post('/canvas', [App\Http\Controllers\ChatsController::class, 'sendCanvas']);
 
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
