11993d35f2571120362153939adf6314c19840e6
Fabian D <fabiandoizifp@ibf.cat>
2024-04-29 19:57:31 +0200

    Created event to broadcast playing word

---
+++ app/Events/SendWord.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SendWord
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $code;
+    public $word;
+
+    public function __construct($code, $word)
+    {
+        $this->code = $code;
+        $this->word = $word;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('room-'.$this->code);
+    }
+
+    public function broadcastAs()
+    {
+        return 'SendWord';
+    }
+}
