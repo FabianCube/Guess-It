8cce392707d804332854b15bc95e846f115cce7e
Fabian <fabian@prueba.com>
2024-05-07 16:40:04 +0200

    removed text from inviteMessage

--- resources/js/utils/swal_notifications.js
+++ resources/js/utils/swal_notifications.js
@@ -73,7 +73,6 @@ export default function sweetAlertNotifications() {
         swal({
             icon: 'info',
             title: $message,
-            text: "¿Deseas unirte a la partida?",
             showCancelButton: true,
             confirmButtonText: 'Aceptar',
             cancelButtonText: 'Rechazar',
