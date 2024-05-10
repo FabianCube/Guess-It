15af1dfbf50573528fd9a828af0047710c7b304d
Rubén Jordán <rubenjordan1a@gmail.comn>
2024-05-10 17:52:17 +0200

    Player list changes order based on points

--- resources/js/views/gameRoom/components/InfoPlayers.vue
+++ resources/js/views/gameRoom/components/InfoPlayers.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div class="p-2 flex flex-column justify-content-start align-items-center players-container">
-        <div v-for="(player, index) in props.players" :key="index" class="p-2 flex flex-column align-items-center player"
+        <div v-for="(player, index) in sortedPlayers" :key="index" class="p-2 flex flex-column align-items-center player"
             :style="{borderColor: player.color}">
             <div class="p-0 m-0 avatar">
                 <img :src="player.avatar" alt="avatar">
@@ -14,12 +14,20 @@
 </template>
 
 <script setup>
-import { defineProps, watch } from 'vue';
+import { defineProps, watch , computed } from 'vue';
 
 const props = defineProps(['players']);
 
 watch(() => props.players, (playersChange) => {
   players.value = playersChange;
+});
+
+// Crear un array ordenado usando una propiedad computada
+const sortedPlayers = computed(() => {
+  // Copiar el array original para no modificar `props.players`
+  const playersCopy = [...props.players];
+  // Ordenar por puntos en orden descendente
+  return playersCopy.sort((a, b) => b.points - a.points);
 });
 
 console.log(props.players);
